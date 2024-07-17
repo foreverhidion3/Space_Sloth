683f119ecf1db7986866db62575d84787ee799fb
root <paul.phlaum@us.af.mil>
2024-07-17 14:26:50 -0600

    Inital commit

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import './main.css'
+import Dancing from './Dancing/Dancing.js';
+
+
+function Main() {
+  
+
+  return (
+    
+     
+      <div className="main">
+      <Dancing />
+        
+      </div>
+   
+  );
+}
+
+export default Main;
+
+
+
+
+
+
+
+
+
